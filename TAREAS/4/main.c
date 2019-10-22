36550f72e107619aa7cb0813f847f7ccdf54f4ab
Dalila Martinez <dalimar1678@gmail.com>
2019-10-22 17:22:54 -0500

    Tarea 4 Modificado
    Agregando comentarios a la tarea 4.

--- TAREAS/4/main.c
+++ TAREAS/4/main.c
@@ -1,13 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+//Este programa recibe una cantidad indefinida de numero como parametros de entrada
 int main(int argc, char *argv[]){
 	double prom, x, res;
 	int i;
 	res=0;
+	//Los parametros de entrada entrar a un ciclo for para sumarse.
+	//El resultado de esa suma se guarda en una variable.
 	for(i=1; i<argc; i++){
 		x=atof(argv[i]);
 		res= (res+x);
 	}
+	//La variable que acumula es dividida entre el numero de argumentos de entrada menos uno.
+	//Es menos uno debido a que consideramos que ./nombre_programa es el primer argumento.
 	prom = (res/(argc-1));
 	printf("%.2f\n", prom);
 	return 0;
