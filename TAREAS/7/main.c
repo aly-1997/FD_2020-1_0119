1cb60d278b9bdd368e7fe1c114f2996ef4b39622
Dalila Martinez <dalimar1678@gmail.com>
2019-11-01 12:41:32 -0600

    Iteracion
    Cambiando la funcion de pow por un ciclo de iteracion. Cambiando comentarios.

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-//En este programa se utiliza tres tipos de librerias
-//Dos de ellas utilizadas anteriormente
-//La libreria de math.h se utiliza debido a que utilizamos
-//la funcion pow, la cual pertenece a esta libreria
+//En este programa se utiliza un ciclo de iteracion para crear la potencia
+//Dado que la potencia es la multiplicacion de un numero n veces
 int main (int argc, char *argv[]){
 	int x,y,pot;
+	pot=1;
 	x=atoi(argv[1]);
 	y=atoi(argv[2]);
+	while(y>0){
+		pot=x*pot;
+		y--;
+	}
 	//Despues de haber convertido a los dos numeros en tipo entero
 	//El primer parametro sera la base y el segundo parametro sera
-	//la potencia a la que se eleva el primer numero
-	pot=pow(x,y);
+	//la potencia a la que se eleva el primer parametro
+	//Esto indica el primer numero sera multiplicado tantas veces 
+	//como el segundo numero indique.
 	printf("%i", pot);
 	return 0;
 }
